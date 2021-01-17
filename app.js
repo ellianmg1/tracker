ff1f9a45e4fb32d66c5bc2566cb2c2c644b32ead
Dipen Saha <ellianmg@outlook.com>
2021-01-17 16:28:11 -0600

    Renamed Views421 folder to Views

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views421'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
